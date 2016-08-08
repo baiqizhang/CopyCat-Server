d64a793b44095e225b0f04b998a31868ca4736af
baiqizhang <baiqiz@andrew.cmu.edu>
2016-08-07 20:10:38 -0700

    minimal version

--- api/v1/router.js
+++ api/v1/router.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const router = new express.Router();
 
-const photos = require('./photos.js');
-const timeline = require('./timeline.js');
-const heartbeat = require('./heartbeat.js');
-const feedback = require('./feedback.js');
-const instagram = require('./socialmedia/instagram.js');
-const reports = require('./reports.js');
-const users = require('./users.js');
+//const photos = require('./photos.js');
+//const timeline = require('./timeline.js');
+//const heartbeat = require('./heartbeat.js');
+//const feedback = require('./feedback.js');
+//const instagram = require('./socialmedia/instagram.js');
+//const reports = require('./reports.js');
+//const users = require('./users.js');
 const smartExperiment = require('./experiment/smart.js');
 const searchExperiment = require('./experiment/search.js');
 
-router.use('/photos', photos);
-router.use('/timeline', timeline);
-router.use('/heartbeat', heartbeat);
-router.use('/feedback', feedback);
-router.use('/instagram', instagram);
-router.use('/reports', reports);
-router.use('/users', users);
+//router.use('/photos', photos);
+//router.use('/timeline', timeline);
+//router.use('/heartbeat', heartbeat);
+//router.use('/feedback', feedback);
+//router.use('/instagram', instagram);
+//router.use('/reports', reports);
+//router.use('/users', users);
 router.use('/smart', smartExperiment);
 router.use('/search', searchExperiment);
-router.use('/crawl', require('./crawl.js'));
+//router.use('/crawl', require('./crawl.js'));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -15,14 +15,14 @@
 
 const express = require('express');
 const server = express();
-const routerV0 = require('./api/v0/router.js');
+//const routerV0 = require('./api/v0/router.js');
 const routerV1 = require('./api/v1/router.js');
 const config = require('./config.js');
 const sprintf = require('sprintf-js').sprintf;
 
-server.use('/api/v0', routerV0);
+//server.use('/api/v0', routerV0);
 server.use('/api/v1', routerV1);
-server.use('/privacy-policy', express.static('privacy-policy'));
+//server.use('/privacy-policy', express.static('privacy-policy'));
 
 // For prod.
 server.listen(config.httpPort, () => {
